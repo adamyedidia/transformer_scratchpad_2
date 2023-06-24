e77d421232d6c55ef974ea321bfbe6ed5a676a69
Adam Yedidia <adamyedidia@gmail.com>
2023-06-24 18:01:58 -0400

    Stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from sklearn.decomposition import PCA
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 from sklearn.preprocessing import StandardScaler
 import pickle
@@ -20,24 +20,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        data = request.get_json()
-        if data is None:
-            # Handle missing data
-            raise Exception('Missing request data')
-
-        layer = data.get('layer')
-        component = data.get('component')
-        diffs_on = data.get('diffs_on')
-        basis_type = data.get('basis_type')
-        
-        if layer is not None:
-            layer = int(layer)
-        if component is not None:
-            component = int(component)
-        if diffs_on is not None:
-            diffs_on = diffs_on.lower() == 'true'
+        layer = int(request.form.get('layer'))
+        component_index = int(request.form.get('component'))
+        diffs_on = 'diffs_on' in request.form  # Returns True if checkbox was checked, False otherwise
+        basis_type = request.form.get('basis_type')
 
         absoluteness_str = '' if diffs_on else 'absolute_'
+
+        resids_by_sentence = pickle.load(open(f'10_token_{absoluteness_str}resids_by_sentence.p', 'rb'))
+
+        sentence_dicts = []
+
         # X = np.array([resids[layer][i][0] for i in range(len(resids[layer]))])
 
         # scaler = StandardScaler()
@@ -67,6 +60,17 @@ def index():
 
         print(component[0])
 
+        if request.form.get('submit_type') == 'Submit Sentence':
+            from andrea_playing_around import get_resids_for_sentence
+            user_sentence = request.form.get('sentence')
+            sentences = [user_sentence] + sentences
+            resids_by_layer = get_resids_for_sentence(user_sentence)
+            for position_index in range(len(enc.encode(user_sentence))):
+                resids_by_sentence[layer][user_sentence].append((
+                    resids_by_layer[layer][0, position_index+1, :].detach().numpy() - resids_by_layer[layer-1][0, position_index+1, :].detach().numpy() if diffs_on and layer > 0 else resids_by_layer[layer][0, position_index+1, :].detach().numpy(), 
+                    position_index+1
+                ))
+
         for sentence in sentences:
             for resid, position in resids_by_sentence[layer][sentence]:
                 resid_scaled = scaler.transform([resid])
@@ -76,6 +80,7 @@ def index():
 
         min_proj = np.min(np.array(raw_projections))
         max_proj = np.max(np.array(raw_projections))
+
 
         for resid_tup, raw_projection in zip(resid_tups, raw_projections):
             
@@ -114,46 +119,13 @@ def index():
         # print(sentence_dicts)
         # print(sentences)
 
-        return render_template('index.html', sentences=sentences, layer=layer, component=component_index, diffs_on=diffs_on, variance_explained=f"{(variance_explained*100):.1f}" if basis_type == 'PCA' else '', basis_type=basis_type, min_proj=min_proj, max_proj=max_proj)
+        user_sentence = None
+
+
+        return render_template('index.html', sentences=sentences, layer=layer, component=component_index, diffs_on=diffs_on, variance_explained=f"{(variance_explained*100):.1f}" if basis_type == 'PCA' else '', basis_type=basis_type)
 
     else:
-        return render_template('index.html', sentences=[], layer=0, component=0, diffs_on=False, variance_explained=0, basis_type='PCA', min_proj=None, max_proj=None)
-
-
-@app.route('/process_sentence', methods=['POST'])
-def process_sentence():
-    from andrea_playing_around import get_resids_for_sentence
-
-    sentence = request.form.get('sentence')
-    layer = int(request.form.get('layer'))
-    component_index = int(request.form.get('component'))    
-    diffs_on = 'diffs_on' in request.form  # Returns True if checkbox was checked, False otherwise
-    basis_type = request.form.get('basis_type')
-    # ... process sentence ...
-
-    diffs_on = 'diffs_on' in request.form  # Returns True if checkbox was checked, False otherwise
-    resids = get_resids_for_sentence(sentence)
-
-    absoluteness_str = '' if diffs_on else 'absolute_'
-
-    component = pickle.load(open(f'10_token_{absoluteness_str}pca_layer_{layer}_component_{component_index}.p', 'rb'))
-    if basis_type == 'Cartesian':
-        component = np.zeros(component.shape)
-        component[component_index] += 1
-    X_pca = pickle.load(open(f'10_token_{absoluteness_str}x_pca_layer_{layer}.p', 'rb'))
-    scaler = pickle.load(open(f'10_token_{absoluteness_str}scaler_layer_{layer}.p', 'rb'))
-
-    resids_for_layer = resids[layer]
-    for i, encoded_word in enumerate(enc.encode(sentence)):
-        resid_for_position = resids_for_layer[0, i+1, :]
-
-
-    # Assuming 'words' is a list of dictionaries, each containing 'text' and 'color' keys.
-    print(layer)
-    print(sentence)
-    print(component_index)
-
-    return jsonify({'words': words})
+        return render_template('index.html', sentences=[], layer=0, component=0, diffs_on=False, variance_explained=0, basis_type='PCA', user_sentence=None)
 
 # for layer in resids:
 #     X = np.array([resids[layer][i][0] for i in range(len(resids[layer]))]) 
